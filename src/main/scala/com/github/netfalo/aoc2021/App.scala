39ed2ec768cd8b1346368c2bdb1ec04caa67d698
Marcell Lipusz <marcell.lipusz@sbab.se>
2021-12-06 12:39:00 +0100

    Remove unnecessary classes

--- src/main/scala/com/github/netfalo/aoc2021/App.scala
+++
@@ -1,5 +0,0 @@
-package example
-
-object AocApp extends App {
-  println("Hello World!")
-}
--- src/main/scala/com/github/netfalo/aoc2021/Window.scala
+++
@@ -1,6 +0,0 @@
-package com.github.netfalo.aoc2020
-
-trait Window {
-  def solveFirstPart(input: String): String
-  def solveSecondPart(input: String): String
-}
